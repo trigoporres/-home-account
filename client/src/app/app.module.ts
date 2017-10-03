f0eea0ca697f58182b4a6b998435ec59001152d7
trigoporres <santiagotrigoporres@gmail.com>
2017-10-03 22:45:45 +0200

    ultimo commit del martes, preparando formulario signup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MySignupFormComponent } from './my-signup-form/my-signup-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MySignupFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
