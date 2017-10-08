df7f10c9bb0846444e86335aca3f42961be484a6
trigoporres <santiagotrigoporres@gmail.com>
2017-10-08 20:43:30 +0200

    en proceso con expenses

--- client/src/app/expenses/expenses.component.ts
+++ client/src/app/expenses/expenses.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+import { AuthService } from '../service/auth.service'
+import { ExpensesService } from '../service/expenses.service'
+
+
+interface expensesInfo{
+  creator: string,
+	name : string,
+	company : string,
+	quantity : number,
+	monthly : boolean,
+	fin : Date,
+	facture : String
+}
 
 @Component({
   selector: 'app-expenses',
@@ -6,10 +21,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expenses.component.css']
 })
 export class ExpensesComponent implements OnInit {
+  user:any;
+  expensesInfo = {
+    creator: this.user._id,
+  	name : "",
+  	company : "",
+  	quantity : "",
+  	monthly : "",
+  	fin : "",
+  	facture : ""
+  }
 
-  constructor() { }
+  constructor(public expenses:ExpensesService, public router: Router, public auth:AuthService) { }
 
   ngOnInit() {
+  }
+
+  create() {
+    this.user = this.auth.getUser();
+    const { name, company, quantity, monthly, fin, facture} = this.expensesInfo;
+    this.expenses.create(this.expensesInfo,this.user)
+    this.router.navigate(['user/'+this.user._id])
   }
 
 }
--- client/src/app/service/expenses.service.ts
+++ client/src/app/service/expenses.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Http} from '@angular/http';
+import 'rxjs';
+import {environment} from '../../environments/environment';
+
+const BASEURL = environment.BASEURL;
+
 
 @Injectable()
 export class ExpensesService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  create(expenses,user){
+
+  }
 
 }
